cd255ec2787e3bd71bbefbc1167510c50acb2b27
Aryan Hegde <hatlermiles69@gmail.com>
2020-11-16 15:13:58 +0530

    nes-emulator

---
+++ nes-emulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NES-emulator
+//
+//  Created by Ryan Hegde on 08/11/20.
+//  Copyright © 2020 Eva Slater. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
